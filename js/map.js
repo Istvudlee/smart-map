0845e2fc138997256898e063a478f072f45eb490
Istvudlee <istvudlee@mail.ru>
2019-02-28 15:51:05 +0300

    mappoint

--- js/map.js
+++ js/map.js
@@ -13,4 +13,33 @@
             // от 0 (весь мир) до 19.
             zoom: 6.5
         });
+        var myPlacemark = new ymaps.Placemark([56.225330311259455, 44.07214804796516], {}, {
+            iconLayout: 'default#image',
+            iconImageHref:'img/placeholder.svg',
+            iconImageSize: [50, 50],
+            iconImageOffset: [-24, -40]
+        });
+        var myPlacemarkred1 = new ymaps.Placemark([56.328085, 44.000291], {}, {
+            iconLayout: 'default#image',
+            iconImageHref:'img/placeholderred.svg',
+            iconImageSize: [50, 50],
+            iconImageOffset: [-24, -40]
+        });
+        var myPlacemarkblue2 = new ymaps.Placemark([55.653350, 37.326303], {}, {
+            iconLayout: 'default#image',
+            iconImageHref:'img/placeholder.svg',
+            iconImageSize: [50, 50],
+            iconImageOffset: [-24, -40]
+        });
+        var myPlacemarkblue3 = new ymaps.Placemark([55.71252, 37.434154], {}, {
+            iconLayout: 'default#image',
+            iconImageHref:'img/placeholder.svg',
+            iconImageSize: [50, 50],
+            iconImageOffset: [-24, -40]
+        });
+        myMap.geoObjects.add(myPlacemark);
+        myMap.geoObjects.add(myPlacemarkred1);
+        myMap.geoObjects.add(myPlacemarkblue2);
+        myMap.geoObjects.add(myPlacemarkblue3);
+
     }
